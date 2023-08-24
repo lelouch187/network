24b09fe578d942ec68404756e492b50d296b9953
lelouch187 <vanushs187@gmail.com>
2023-08-24 22:04:22 +0600

    feat: add color and fonts

---
+++ constant/colors.ts
@@ -0,0 +1,25 @@
+export const Colors = {
+  Light_700: '#131313',
+  Light_400: '#696969',
+  Light_300: '#9B9B9B',
+  Light_250: '#D0D1D0',
+  Light_200: '#DEDEDE',
+  Light_150: '#F4F5F4',
+  Light_100: '#FFFFFF',
+
+  Dark_100: '#FFFFFF',
+  Dark_200: '#DEDEDE',
+  Dark_300: '#9B9B9B',
+  Dark_400: '#696969',
+  Dark_500: '#303030',
+  Dark_600: '#1b1b1b',
+  Dark_700: '#131313',
+
+  Light_primary_default: '#02FF3A',
+  Light_primary_pressed: '#75C537',
+
+  Dark_primary_default: '#91C521',
+  Dark_primary_pressed: '#7AA818',
+
+  Error: '#C2534C',
+};
